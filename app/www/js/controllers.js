bfc2c906da6fcf86675f6a3cdec8c5b93d10ffa7
2BoysAndHats <2BoysAndHats>
2016-11-28 19:33:18 +0000

    Ionic tab app, with some changes.
    The tabs are renamed, and there is only one tab file that uses templating

---
+++ app/www/js/controllers.js
@@ -0,0 +1,13 @@
+angular.module('starter.controllers', [])
+
+.controller('GreenCtrl', function($scope) {
+	$scope.title = "Green";
+})
+
+.controller('AmberCtrl', function($scope) {
+  	$scope.title = "Amber";
+})
+
+.controller('RedCtrl', function($scope) {
+  	$scope.title = "Red";
+});
