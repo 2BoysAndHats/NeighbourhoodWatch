31a524b4122ef59ca99280365545236e2362f7e4
2BoysAndHats <2BoysAndHats>
2016-11-29 16:17:19 +0000

    Remove images and add email support

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -1,13 +1,44 @@
 angular.module('starter.controllers', [])
 
-.controller('GreenCtrl', function($scope) {
+.controller('GreenCtrl', function($scope,Email) {
 	$scope.title = "Green";
+
+	$scope.formdata = {
+		name:"",
+		location:"",
+		comment:""
+	};
+
+	$scope.submit = function () {
+		 Email.sendEmail($scope);
+	}
+
 })
 
-.controller('AmberCtrl', function($scope) {
+.controller('AmberCtrl', function($scope,Email) {
   	$scope.title = "Amber";
+
+  	$scope.formdata = {
+  		name:"",
+		location:"",
+		comment:""
+	};
+
+	$scope.submit = function () {
+		Email.sendEmail($scope);
+	}
 })
 
-.controller('RedCtrl', function($scope) {
+.controller('RedCtrl', function($scope,Email) {
   	$scope.title = "Red";
+
+  	$scope.formdata = {
+  		name:"",
+		location:"",
+		comment:""
+	};
+
+	$scope.submit = function () {
+		Email.sendEmail($scope);	
+	}
 });
